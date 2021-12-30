659554cbacb1cbf3add65ecb315738961e7451e8
Joel <joelclementes.dev@gmail.com>
2021-12-30 14:32:51 -0600

    agregando favicon.ico

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if ($p == "atencionpersonal") {
 
 <head>
   <title>Gestión de documentos</title>
-
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
